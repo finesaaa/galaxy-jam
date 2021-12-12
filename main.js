08b3f8494408159a97059d71ae719bf775d1116b
maroqijalil <jalilpro31@gmail.com>
2021-12-12 14:49:11 +0700

    init: build project

---
+++ main.js
@@ -0,0 +1,59 @@
+var camera, scene, renderer, stars = [];
+
+function init() {
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 5;
+
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+}
+
+
+function addSphere() {
+  for (var z = -1000; z < 1000; z += 20) {
+
+    var geometry = new THREE.SphereGeometry(0.5, 32, 32)
+    var material = new THREE.MeshBasicMaterial({
+      color: 0xffffff
+    });
+    var sphere = new THREE.Mesh(geometry, material)
+
+    sphere.position.x = Math.random() * 1000 - 500;
+    sphere.position.y = Math.random() * 1000 - 500;
+
+    sphere.position.z = z;
+
+    sphere.scale.x = sphere.scale.y = 2;
+
+    scene.add(sphere);
+
+    stars.push(sphere);
+  }
+}
+
+function animateStars() {
+  for (var i = 0; i < stars.length; i++) {
+
+    star = stars[i];
+
+    star.position.z += i / 100;
+
+    if (star.position.z > 1000) star.position.z -= 2000;
+  }
+}
+
+function render() {
+  requestAnimationFrame(render);
+
+  renderer.render(scene, camera);
+  animateStars();
+
+}
+
+init();
+addSphere();
+render();
