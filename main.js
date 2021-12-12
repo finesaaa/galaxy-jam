bd32da49880f53abab7f1b745d602f806db51e34
maroqijalil <jalilpro31@gmail.com>
2021-12-12 18:58:57 +0700

    feat (main): add animation

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ import {
 } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 var camera, scene, renderer, stars = [];
+var clock, mixer;
 
 function init() {
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
   camera.position.z = 5;
 
   scene = new THREE.Scene();
@@ -18,6 +19,7 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
+  clock = new THREE.Clock();
 
   const loader = new GLTFLoader()
   loader.load('models/rocket/scene.gltf', function (gltf) {
@@ -27,8 +29,39 @@ function init() {
     scene.add(root)
   });
 
+  loader.load('models/mercury/scene.gltf', function (gltf) {
+    const root = gltf.scene
+    root.scale.set(0.1, 0.1, 0.1)
+    root.position.set(-2, 0, 0)
+    scene.add(root)
+  });
+
+  loader.load('models/venus/scene.gltf', function (gltf) {
+    const root = gltf.scene
+    root.scale.set(0.1, 0.1, 0.1)
+    root.position.set(2, 0, -100)
+    scene.add(root)
+  });
+
+  loader.load('models/earth/scene.gltf', function (gltf) {
+    const root = gltf.scene;
+    root.scale.set(0.1, 0.1, 0.1);
+    root.position.set(2, 0, -100);
+    scene.add(root);
+
+    mixer = new THREE.AnimationMixer(root);
+    mixer.clipAction(gltf.animations[0]).play();
+  });
+
+  // loader.load('models/mars/scene.gltf', function (gltf) {
+  //   const root = gltf.scene
+  //   root.scale.set(0.1, 0.1, 0.1)
+  //   root.position.set(-2, 0, -1000)
+  //   scene.add(root)
+  // });
+
   const light = new THREE.PointLight(0xffffff , 1)
-  light.position.set(2,2,5)
+  light.position.set(2, 2, 5)
   scene.add(light)
 }
 
@@ -47,7 +80,7 @@ function addSphere() {
 
     sphere.position.z = z;
 
-    sphere.scale.x = sphere.scale.y = 2;
+    sphere.scale.x = sphere.scale.y = 0.5;
 
     scene.add(sphere);
 
@@ -66,6 +99,8 @@ function animateStars() {
       star.position.z -= 2000;
     }
   }
+
+  mixer.update(clock.getDelta());
 }
 
 function render() {
